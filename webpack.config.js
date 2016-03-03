c1c1ad3ebd2a5d035822968f65bfe2f856d8ee04
Oliver Wise <owise1@gmail.com>
2016-03-03 09:48:40 -0800

    init commit :blush:

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    entry: "./src/entry.js",
+    output: {
+        path: './public',
+        filename: "bundle.js",
+        publicPath : "/public/"
+    },
+    module: {
+        loaders: [
+           { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
+        ]
+    }
+}
