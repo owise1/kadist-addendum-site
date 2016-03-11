82ffc4488681b505d56f626cd77f57ef1c8642e4
Oliver Wise <owise1@gmail.com>
2016-03-11 12:48:27 -0800

    :feet:

--- webpack.config.js
+++ webpack.config.js
@@ -5,16 +5,16 @@ var webpack = require('webpack')
 module.exports = {
     entry: "./src/entry.js",
     output: {
-        path: './public',
+        path: 'public/',
         filename: "bundle.js",
-        publicPath : "/public/"
+        publicPath : "/"
     },
     module: {
         loaders: [
            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
            { test: /\.less$/, loader: "style!css!postcss-loader!less" },
            { test: /\.html$/, loader: "raw-loader" },
-           { test: /(\.jpg)|(\.svg)$/,    loader: "file-loader" }
+           { test: /(\.jpg)|(\.svg)$/,    loader: "file?name=[name].[ext]" }
         ]
     },
     postcss: function () {
